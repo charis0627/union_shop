2cb605b999a69ae7c816c069c3667a9766fc55eb
charis0627 <charisva27@gmail.com>
2025-11-22 17:51:22 +0000

    Add About Us page

---
+++ lib/about_us_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AboutUsPage extends StatelessWidget {
+  const AboutUsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Us'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'About Us',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Welcome to the Union Shop!\n\nWe are dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\nAll online purchases are available for delivery or instore collection!\n\nWe hope you enjoy our products as much as we enjoy offering them to you.\n\nHappy shopping! The Union Shop & Reception Team​​​​​​​​​',
+                style: TextStyle(fontSize: 16, height: 1.4),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
